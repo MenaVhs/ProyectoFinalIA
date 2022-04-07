6b13d24dcadebd34c4f52b5695eafb8858a5f2d2
Angel Eduardo Gomez Aburto <uvangel.gomez@gmail.com>
2022-04-07 18:38:28 -0500

    resize image

---
+++ preparar_imagenes.py
@@ -0,0 +1,16 @@
+from turtle import width
+import cv2
+import os
+ 
+path = r'./img'
+
+lista = os.listdir(path)
+
+for i in os.listdir(path):
+    img = cv2.imread(path+"\\"+i)
+    valor = 2
+    width = int(img.shape[1]*valor)
+    height = int(img.shape[0]*valor)
+    dim = (width, height)
+    resized = cv2.resize(img, dim)
+    cv2.imwrite(path+"\\"+i, resized)
