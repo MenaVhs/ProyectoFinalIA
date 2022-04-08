bcddf5fc347c4e61ec99968f88067292cb850304
Angel Eduardo Gomez Aburto <uvangel.gomez@gmail.com>
2022-04-07 19:28:15 -0500

    2do script recortar

--- preparar_imagenes.py
+++ preparar_imagenes.py
@@ -1,4 +1,3 @@
-from turtle import width
 import cv2
 import os
  
@@ -8,9 +7,10 @@ lista = os.listdir(path)
 
 for i in os.listdir(path):
     img = cv2.imread(path+"\\"+i)
-    valor = 2
-    width = int(img.shape[1]*valor)
-    height = int(img.shape[0]*valor)
-    dim = (width, height)
+    #valor = 2
+    #width = int(img.shape[1]*valor)
+    #height = int(img.shape[0]*valor)
+    #dim = (width, height)
+    dim = (64, 64)
     resized = cv2.resize(img, dim)
     cv2.imwrite(path+"\\"+i, resized)
