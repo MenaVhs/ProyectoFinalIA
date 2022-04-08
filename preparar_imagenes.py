744676c7ba7e6bc62c7e96e81a9327d93754e428
Angel Eduardo Gomez Aburto <uvangel.gomez@gmail.com>
2022-04-07 20:44:28 -0500

    comentado

--- preparar_imagenes.py
+++ preparar_imagenes.py
@@ -1,17 +1,23 @@
+#importar librerías
 import cv2
 import os
  
+#crear variable de la ruta donde están las imágenes
 path = r'./img'
 
+#crear una array con el nombre de todos los archivos y carpetas
+#dentro de path
 lista = os.listdir(path)
 
-for i in os.listdir(path):
+#iterar la lista
+for i in lista:
+    #concatena la ruta + nombre de la imagen
     img = cv2.imread(path+"\\"+i)
-    #valor = 2
-    #width = int(img.shape[1]*valor)
-    #height = int(img.shape[0]*valor)
-    #dim = (width, height)
-    dim = (64, 64)
+    #imagen de 64 px x 64 px
+    dimensiones = (64, 64)
+    #recorte de la imágen
     img = img[60:1000, 430:1370]
-    resized = cv2.resize(img, dim)
+    #reescala la imagen recortada
+    resized = cv2.resize(img, dimensiones)
+    #reescribe la imagen recortada y escalada por la original
     cv2.imwrite(path+"\\"+i, resized)
